993fc7db5a717b3cd932bee0517c8a56f867a28e
qiang.sheng <qiang.sheng@godinsec.com>
2015-06-05 19:44:49 +0800

    add linux command exec md5sum

--- md5.go
+++ md5.go
@@ -3,9 +3,32 @@ package md5
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"io"
 	"os"
+	"os/exec"
+	"strings"
+	"unsafe"
 )
+
+type MD5Info struct {
+	Md5  string
+	File string
+}
+
+func (this *MD5Info) Write(buff []byte) (int, error) {
+	info := *(*string)(unsafe.Pointer(&buff))
+
+	infos := strings.Split(info, " ")
+	if len(infos) < 2 {
+		return 0, errors.New("get md5 error")
+	}
+
+	this.Md5 = infos[0]
+	this.File = infos[1]
+
+	return len(buff), nil
+}
 
 func MD5(buff []byte) ([]byte, error) {
 	h := md5.New()
@@ -54,4 +77,20 @@ func FileMD5(filename string) (string, error) {
 	}
 
 	return hex.EncodeToString(h), nil
+}
+
+func MD5CMD(file string) (string, []byte, error) {
+	cmd := exec.Command("md5sum", file)
+
+	info := &MD5Info{}
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = info
+
+	err := cmd.Run()
+	if err != nil {
+		return "", nil, err
+	}
+
+	return info.Md5, *(*[]byte)(unsafe.Pointer(&info.Md5)), nil
 }
--- md5_test.go
+++ md5_test.go
@@ -1,27 +1,10 @@
 package md5
 
 import (
-	"encoding/hex"
 	"github.com/Congenital/log/v0.2/log"
-	"os"
 	"testing"
 )
 
 func TestMD5(t *testing.T) {
-	h, err := MD5([]byte("fdsafdsa"))
-
-	log.Info(hex.EncodeToString(h), err)
-
-	file, err := os.Open("md5.go")
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	defer file.Close()
-
-	he, err := IoMD5(file)
-
-	log.Info(hex.EncodeToString(he), err)
-
-	log.Info(FileMD5("md5.go"))
+	log.Info(MD5CMD("/home/andy/Downloads/STAX人头录音专辑.zip"))
 }
